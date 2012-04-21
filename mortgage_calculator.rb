1cc9d4be7eb05142846111fe5d065a8b84cb95a1
Chris Tonog <chris.tonog@gmail.com>
2012-04-21 16:23:56 -0400

    added twitter bootstrap and added mortgage calculation

--- mortgage_calculator.rb
+++ mortgage_calculator.rb
@@ -1,21 +1,42 @@
 require 'sinatra'
 
+helpers do
+	#no idea how the below method reaches the comma thing, copied and pasted from
+	#http://johnallen.us/?p=380
+	def add_commas_to_number(large_number)
+		large_number.to_s =~ /([^\.]*)(\..*)?/
+		int, dec = $1.reverse, $2 ? $2 : ""
+		while int.gsub!(/(,|\.|^)(\d{3})(\d)/, '\1\2,\3')
+		end
+		int.reverse + dec
+	end
+end
+
 get '/' do
 
-	erb :calculator	
+	@title = "Reversed Mortgage Calculator"
+	erb :home	
 
 end
 
 post '/' do
 
+	@title = "Your results"
+	interest_rate = params[:interest_rate].to_f
+	
+	monthly_payment = params[:monthly_payment].to_i 
+	interest_rate_per_period = interest_rate/12.0
+	number_of_payments = 360 
+	@loan_amount = (monthly_payment/interest_rate_per_period) * (1-(1+interest_rate_per_period)**-360) 
 	erb :results
 
 end
 
 get '/about' do
 
-	"a Chris Tonog production"
-	
+	@title = "About the App"
+	erb :about
+
 end
 
 =begin
